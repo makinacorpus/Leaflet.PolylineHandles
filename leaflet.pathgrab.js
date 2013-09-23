afcfc7a12092b54cd9738db57ab11e80294b8595
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-09-23 15:16:32 +0200

    Snap marker on dragging

--- leaflet.pathgrab.js
+++ leaflet.pathgrab.js
@@ -16,6 +16,7 @@ L.Handler.PolylineGrab = L.Handler.extend({
         this._map = map;
         this._layers = [];
         this._previous = null;
+        this._dragging = false;
 
         var grabIcon = L.divIcon({className: 'grab-icon'});
         this._marker = L.marker([0, 0], {icon: grabIcon});
@@ -24,8 +25,9 @@ L.Handler.PolylineGrab = L.Handler.extend({
         this.__mouseMoveSampling = (function () {
             var timer = new Date();
             return function (e) {
-                var date = new Date();
-                if ((date - timer) < L.Handler.PolylineGrab.SAMPLING_PERIOD) {
+                var date = new Date(),
+                    filtered = (date - timer) < L.Handler.PolylineGrab.SAMPLING_PERIOD;
+                if (this._dragging || filtered) {
                     return;  // Ignore movement
                 }
                 timer = date;
@@ -37,9 +39,13 @@ L.Handler.PolylineGrab = L.Handler.extend({
     addHooks: function () {
         this._map.on('mousemove', this.__mouseMoveSampling, this);
         this._map.on('mousemovesample', this._onMouseMove, this);
+        this.on('grab:on', this._onGrabOn, this);
+        this.on('grab:off', this._onGrabOff, this);
     },
 
     removeHooks: function () {
+        this.off('grab:on', this._onGrabOn, this);
+        this.off('grab:off', this._onGrabOff, this);
         this._map.off('mousemovesample');
         this._map.off('mousemove', this.__mouseMoveSampling, this);
     },
@@ -62,18 +68,51 @@ L.Handler.PolylineGrab = L.Handler.extend({
 
         if (closest) {
             if (!this._previous) {
+                this._marker.addTo(this._map);
                 this.fire('grab:on', {layer: this});
             }
-            this._marker.setLatLng(closest.latlng).addTo(this._map);
-            this.fire('grab:move', {layer: this, latlng: e.latlng});
+            this._marker.setLatLng(closest.latlng);
+            this.fire('grab:move', {marker: this._marker,
+                                    layer: this,
+                                    latlng: e.latlng});
         }
         else {
-            this._map.removeLayer(this._marker);
             if (this._previous) {
                 this.fire('grab:off', {layer: this});
+                this._map.removeLayer(this._marker);
             }
         }
         this._previous = closest;
+    },
+
+    _onGrabOn: function (e) {
+        this._marker.snapediting = new L.Handler.MarkerSnap(this._map);
+        for (var i=0, n=this._layers.length; i<n; i++) {
+            this._marker.snapediting.addGuideLayer(this._layers[i]);
+        }
+
+        this._marker.dragging = new L.Handler.MarkerDrag(this._marker);
+        this._marker.dragging.enable();
+        this._marker.on('dragstart', this._onDragStart, this);
+        this._marker.on('dragend', this._onDragEnd, this);
+    },
+
+    _onGrabOff: function (e) {
+        this._marker.off('dragstart', this._onDragStart, this);
+        this._marker.off('dragend', this._onDragEnd, this);
+        delete this._marker.snapediting;
+    },
+
+    _onDragStart: function (e) {
+        this._marker.snapediting.watchMarker(this._marker);
+        this._marker.snapediting.enable();
+        this._dragging = true;
+    },
+
+    _onDragEnd: function (e) {
+        this._dragging = false;
+        this._marker.snapediting.unwatchMarker(this._marker);
+        this._marker.snapediting.disable();
     }
 });
 
